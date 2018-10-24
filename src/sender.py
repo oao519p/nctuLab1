d59ac1d12e19dc0644cefa562cd8a30851e97a0d
oao519p <34864107+oao519p@users.noreply.github.com>
2018-10-24 17:50:35 +0800

    correct problem 3

--- src/sender.py
+++ src/sender.py
@@ -23,7 +23,7 @@ def main():
     # Define customized header (Task 3.)p
     my_id = '0616211'
     my_dept = 'cs'
-    my_gender = 'female'
+    my_gender = 2
     student = Protocol(id = my_id, dept = my_dept, gender = my_gender)
 
     # Read file and store into list
